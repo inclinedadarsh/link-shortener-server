b6d7321b8d9a16af185e9dc13d4a84a4279e9aec
Adarsh Dubey <dubeyadarshmain@gmail.com>
2025-05-18 20:30:08 +0530

    feat: add sqlmodel and initialize database
    - add sqlmodel and python-dotenv deps
    - create db.py with all sqlmodel logic
    - add env variables
    - add env variables in .gitignore
    - create lifespan for env variables

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from .db import create_db_and_tables
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application is starting up...")
+    create_db_and_tables()
+
+    yield
+    print("Application is closing...")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
